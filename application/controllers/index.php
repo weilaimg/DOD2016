2474c746028fc2dffeccef7cea6952a789178b27
weilaimg1314 <weilaimg1314@gmail.com>
2016-07-30 17:03:02 +0800

    调试

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -163,9 +163,10 @@ class Index extends CI_Controller {
 
 		$this -> load -> model ('login_model','login');
 		$uid = $this -> login -> check_by_open_id($openid);
+		p($uid);die;
 		if(count($uid)){
 			$data = array(
-				'nickname' = $userinfo['nickname'],
+				'nickname' = $userinfo['nickname']
 				);
 			$this -> login -> update_userinfo_by_uid($uid,$data);
 			$_SESSION['uid'] = $uid;
